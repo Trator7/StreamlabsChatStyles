11e8e8bf52374e057dd84735956aa9650e7fb001
trator <jose7juan@hotmail.com>
2023-01-22 01:01:08 +0100

    Upload new chat style and index.html

--- Source/Logic.js
+++ Source/Logic.js
@@ -8,3 +8,17 @@ document.addEventListener('onEventReceived', function(obj) {
   	// obj will contain information about the event
 	
 });
+
+function openTab(evt, tabName) {
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+	document.getElementById(tabName).style.display = "block";
+	evt.currentTarget.className += " active";
+}
---
+++ wwwroot/js/global.js
@@ -0,0 +1,36 @@
+function openTab(elem, tabName) {
+	var streamlabStyle = elem.parentElement.parentElement;
+	var buttons = elem.parentElement.querySelectorAll("button");
+	for(var i = 0; i < buttons.length; i++){
+		buttons[i].className = "tablinks";
+	}
+
+	var allTabs = streamlabStyle.querySelectorAll(".tabcontent");
+	for(var i = 0; i < allTabs.length; i++){
+		allTabs[i].hidden = true;
+	}
+
+	streamlabStyle.querySelector("#"+tabName).hidden = false
+	elem.className += " active";
+}
+
+function copyToClipboard(elem) {
+	var streamlabStyle = elem.parentElement.parentElement;
+	var allTabs = streamlabStyle.querySelectorAll(".tabcontent");
+	for(var i = 0; i < allTabs.length; i++){
+		if(allTabs[i].hidden == false)
+		{
+			const el = document.createElement("textarea");
+			var txt = allTabs[i].innerHTML;
+			txt = txt.replaceAll('&lt;', '<');
+			txt = txt.replaceAll('&gt;', '>');
+			txt = txt.replaceAll('<br>', '');
+			txt = txt.replaceAll('                    ', '');
+			el.value = txt;
+			document.body.appendChild(el);
+			el.select();
+			document.execCommand("copy");
+			document.body.removeChild(el);
+		}
+	}
+}
